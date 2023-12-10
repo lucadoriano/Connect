59beb6b1ba3c89861e08e8cfdb845cb2d6d6ac12
Luca D'Oriano <luca.doriano1@gmail.com>
2023-12-11 00:59:20 +0100

    Added private messages

--- src/app/core/models.py
+++ src/app/core/models.py
@@ -1,10 +1,13 @@
 import uuid
 
+from datetime import datetime
+from pytz import timezone
+
 from flask_login import UserMixin
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-from sqlalchemy import Boolean, UUID, Text, String, ForeignKey
+from sqlalchemy import Boolean, UUID, Integer, String, ForeignKey, DateTime
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -16,9 +19,12 @@ class User(db.Model, UserMixin):
     email = db.Column(String(120), unique=True)
     username = db.Column(String(60), nullable=True)
     password = db.Column(String(300), nullable=False)
-    tutor = db.Column(Boolean, default=False)
+    
     profile = db.relationship(
-        'Profile', backref='user', uselist=False, lazy='joined',
+        'Profile', 
+        backref='user', 
+        uselist=False, 
+        lazy='joined',
     )
 
     def __init__(self, email, password, username):
@@ -34,6 +40,10 @@ class User(db.Model, UserMixin):
     @classmethod
     def find_by_id(cls, id: uuid):
         return cls.query.filter_by(id=id).first()
+    
+    @classmethod
+    def find_by_username(cls, username: str):
+        return cls.query.filter_by(username=username).first()
     
     @classmethod
     def find_by_email(cls, email: str):
@@ -52,16 +62,67 @@ class User(db.Model, UserMixin):
 class Profile(db.Model):
     __tablename__ = 'profile'
 
-    id = db.Column(UUID, primary_key=True, default=uuid.uuid4)
+    id = db.Column(Integer, primary_key=True)
     user_id = db.Column(UUID, ForeignKey('user.id'), unique=True, nullable=False)
     image = db.Column(String(), nullable=True)
     fullname = db.Column(String(100), nullable=True)
     description = db.Column(String(100), nullable=True)
     about = db.Column(String(500), nullable=True)
     skills = db.Column(String(500), default='', nullable=True)
+    tutor = db.Column(Boolean, default=False)
+
+    messages_sent = db.relationship(
+        'Message', 
+        back_populates='sender_profile',
+        foreign_keys='Message.sender_id'
+    )
+    messages_received = db.relationship(
+        'Message', 
+        back_populates='recipient_profile',
+        foreign_keys='Message.recipient_id'
+    )
 
     def __init__(self, user_id):
         self.user_id = user_id
     
     def __repr__(self):
-        return f'<Profile [{self.id}] - {self.fullname}'
+        return f'<Profile [{self.id}] - {self.fullname}>'
+    
+
+class Message(db.Model):
+    id = db.Column(Integer, primary_key=True)
+    sender_id = db.Column(UUID, ForeignKey('profile.user_id'), nullable=False)
+    recipient_id = db.Column(UUID, ForeignKey('profile.user_id'), nullable=False)
+    body = db.Column(String(150), nullable=False)
+    timestamp = db.Column(DateTime(timezone=True))
+
+    sender_profile = db.relationship(
+        'Profile', 
+        back_populates='messages_sent',
+        foreign_keys='Message.sender_id'
+    )
+    recipient_profile = db.relationship(
+        'Profile',
+        back_populates='messages_received',
+        foreign_keys='Message.recipient_id'
+    )
+
+    def __init__(self, sender_id, recipient_id, body):
+        self.sender_id = sender_id
+        self.recipient_id = recipient_id
+        self.body = body
+        self.timestamp = datetime.now(tz=timezone('Europe/Rome'))
+
+    def __repr__(self):
+        return f'<Message [{self.id}]>'
+
+
+    def save(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+
+        except Exception as e:
+            db.session.rollback()
+            db.session.close()
+            raise e
---
+++ src/app/static/js/inbox.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const receivedMessageList = document.getElementById('receivedMessageList');
+    const sentMessageList = document.getElementById('sentMessageList');
+    const selectedMessageContent = document.getElementById('selectedMessageContent');
+    
+    function handleTabClick(messageList) {
+        messageList.addEventListener('click', function (event) {
+            const listItem = event.target.closest('li');
+
+            if (listItem) {
+                const sender = listItem.dataset.sender;
+                const messages = listItem.dataset.messages.split(',');
+                const timestamp = listItem.dataset.timestamp;
+                const label = listItem.dataset.label;
+
+                // Updates the selected conversation on the right
+                $('#selected-card-body').removeClass('d-flex align-items-center justify-content-center')
+                selectedMessageContent.innerHTML = `<h6 class="text-muted">${timestamp}</h6><h5>${label}: ${sender}</h5><hr>`;
+
+                messages.forEach(message => {
+                    const messageDiv = document.createElement('div');
+                    messageDiv.textContent = message.trim();
+                    selectedMessageContent.appendChild(messageDiv);
+                });
+
+                // Remove active class from other list items
+                const listItems = messageList.getElementsByTagName('li');
+                for (const item of listItems) {
+                    item.classList.remove('active');
+                }
+
+                // Add active class to the clicked list item
+                listItem.classList.add('active');
+            }
+        });
+    }
+    // Handles click events for both tabs
+    handleTabClick(receivedMessageList);
+    handleTabClick(sentMessageList);
+});
+
+$(document).ready(function () {
+    const path = window.location.pathname.split('/');
+    const username = path.length > 2 ? [path.length - 1] : null;
+    const urlUsername = path[2]
+
+    // Check if a username is extracted
+    if (username) {
+        // Opens the modal if a username is in the path
+        $('#staticBackdrop').modal('show');
+        $('#recipient').val(urlUsername)
+    }
+});
